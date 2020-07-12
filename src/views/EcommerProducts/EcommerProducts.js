84e4ab5f9c33b7a7d88c546490364c9da6e052b2
keyberth rengel <keyrengel24@gmail.com>
2020-07-11 23:19:24 -0400

    se coloco un condicion en el useEffect de ecommerProducts

--- src/views/EcommerProducts/EcommerProducts.js
+++ src/views/EcommerProducts/EcommerProducts.js
@@ -4,57 +4,55 @@ import { useDispatch, useSelector } from "react-redux";
 import { Grid } from "@material-ui/core";
 
 import { editproducts } from "../../store/Products/actions";
-import ModalScreenNuevoProd from '../../components/Molecules/ModalScreenNuevoProd/index'
+import ModalScreenNuevoProd from "../../components/Molecules/ModalScreenNuevoProd/index";
 
 import TableProduct from "../../components/Organisms/FormIngredientes/table";
-import { editAddMenu, filterMenu, filterMenuOnline } from "../../store/AgregarMenu/actions";
+import {
+  editAddMenu,
+  filterMenu,
+  filterMenuOnline,
+} from "../../store/AgregarMenu/actions";
 import ProductCardViewColumn from "../../components/Molecules/ProductCard/variants/ViewColumn";
 
 const EcommerProducts = () => {
-    const dispatch = useDispatch();
-    const dataProducts = useSelector((state) => state.addmenu.menuFilterOnline);
-    //const productosIngredientes = useSelector((state) => state.products.products);
-    const productosAllmenu = useSelector((state) => state.addmenu.addmenu);
-    const productosIngredientes = useSelector((state) => state.products.products);
+  const dispatch = useDispatch();
+  const dataProducts = useSelector((state) => state.addmenu.menuFilterOnline);
+  //const productosIngredientes = useSelector((state) => state.products.products);
+  const productosAllmenu = useSelector((state) => state.addmenu.addmenu);
+  const productosIngredientes = useSelector((state) => state.products.products);
 
-    const activarFiltro = async () => {
+  const activarFiltro = async () => {};
+  useEffect(() => {
+    if (productosAllmenu === []) {
+      dispatch(filterMenuOnline(productosAllmenu, "online"));
     }
-    useEffect(async () => {
+  }, [productosAllmenu]);
 
-        await dispatch(filterMenuOnline(productosAllmenu, "online"));
+  //const dataProducts = useSelector((state) => state.addmenu.menufilter);
+  const data = Object.keys(dataProducts).map((i) => {
+    // console.log(dataProducts[i]);
+    dataProducts[i].id = i;
 
-    }, [dispatch]);
+    return dataProducts[i];
+  });
 
-    //const dataProducts = useSelector((state) => state.addmenu.menufilter);
-    const data = Object.keys(dataProducts).map((i) => {
-        // console.log(dataProducts[i]);
-        dataProducts[i].id = i;
+  let gridSize = {
+    xs: 12,
+    sm: 4,
+    md: 3,
+    lg: 2,
+    xl: 2,
+  };
 
-        return dataProducts[i];
-    });
-
-    let gridSize = {
-        xs: 12,
-        sm: 4,
-        md: 3,
-        lg: 2,
-        xl: 2,
-    };
-
-    return (
-        <div>
-            {data.map((item, index) => (
-                <Grid
-                    item
-                    gridSize={12}
-                    key={index}
-
-                >
-                    <ProductCardViewColumn variant={"column"} products={item} />
-                </Grid>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {data.map((item, index) => (
+        <Grid item gridSize={12} key={index}>
+          <ProductCardViewColumn variant={"column"} products={item} />
+        </Grid>
+      ))}
+    </div>
+  );
 };
 
 export default EcommerProducts;
