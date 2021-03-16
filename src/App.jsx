e95cb9c5db9b7a7eac26c558885cf0c6c1baa343
ProboTector <programador@gnt.pe>
2021-03-16 17:05:15 -0500

    install

--- src/App.jsx
+++ src/App.jsx
@@ -29,40 +29,40 @@ import Usuarios from "./views/users";
 import { AllReserved, AddReserved } from "./views/reserved";
 
 const App = () => {
-  // if (loggedIn) {
-  return (
-    <Provider store={store}>
-      <AuthLoading>
-        <BrowserRouter>
-          <Switch>
-            <ProtectedRouter exact path="/" component={DashboardView} />
-            <ProtectedRouter exact path="/crear-menu" component={AllProducts} />
-            <ProtectedRouter exact path="/all-menu" component={AllMenu} />
-            <ProtectedRouter exact path="/pedidos" component={Pedidos} />
-            <ProtectedRouter exact path="/users" component={Usuarios} />
-            <ProtectedRouter exact path="/lista-ordenes" component={ListaPedidos} />
-            <ProtectedRouter exact path="/ingredientes" component={FormIngredientes} />
-            <ProtectedRouter exact path="/calcular-pago" component={Calculadora} />
-            <ProtectedRouter exact path="/clients" component={Clients} />
+	// if (loggedIn) {
+	return (
+		<Provider store={store}>
+			<AuthLoading>
+				<BrowserRouter>
+					<Switch>
+						<ProtectedRouter exact path="/" component={DashboardView} />
+						<ProtectedRouter exact path="/crear-menu" component={AllProducts} />
+						<ProtectedRouter exact path="/all-menu" component={AllMenu} />
+						<ProtectedRouter exact path="/pedidos" component={Pedidos} />
+						<ProtectedRouter exact path="/users" component={Usuarios} />
+						<ProtectedRouter exact path="/lista-ordenes" component={ListaPedidos} />
+						<ProtectedRouter exact path="/ingredientes" component={FormIngredientes} />
+						<ProtectedRouter exact path="/calcular-pago" component={Calculadora} />
+						<ProtectedRouter exact path="/clients" component={Clients} />
 
-            <ProtectedRouter exact path="/reportes" component={Reportes} />
-            <ProtectedRouter exact path="/tables" component={AllTables} />
-            <ProtectedRouter exact path="/addTable" component={AddTable} />
-            <ProtectedRouter exact path="/sectors" component={AllSectors} />
-            <ProtectedRouter exact path="/addSector" component={AddSector} />
-            <ProtectedRouter exact path="/deliverys" component={AllDeliverys} />
-            <ProtectedRouter exact path="/kitchens" component={AllKitchens} />
-            <ProtectedRouter exact path="/bars" component={AllBars} />
-            <ProtectedRouter exact path="/reserved" component={AllReserved} />
-            <ProtectedRouter exact path="/addReserved" component={AddReserved} />
-            <Route exact path="/sign-in" component={SignInView} />
-            <Route exact path="/sign-up" component={SignUpView} />
-            <Route component={Error404} />
-          </Switch>
-        </BrowserRouter>
-      </AuthLoading>
-    </Provider>
-  );
+						<ProtectedRouter exact path="/reportes" component={Reportes} />
+						<ProtectedRouter exact path="/tables" component={AllTables} />
+						<ProtectedRouter exact path="/addTable" component={AddTable} />
+						<ProtectedRouter exact path="/sectors" component={AllSectors} />
+						<ProtectedRouter exact path="/addSector" component={AddSector} />
+						<ProtectedRouter exact path="/deliverys" component={AllDeliverys} />
+						<ProtectedRouter exact path="/kitchens" component={AllKitchens} />
+						<ProtectedRouter exact path="/bars" component={AllBars} />
+						<ProtectedRouter exact path="/reserved" component={AllReserved} />
+						<ProtectedRouter exact path="/addReserved" component={AddReserved} />
+						<Route exact path="/sign-in" component={SignInView} />
+						<Route exact path="/sign-up" component={SignUpView} />
+						<Route component={Error404} />
+					</Switch>
+				</BrowserRouter>
+			</AuthLoading>
+		</Provider>
+	);
 };
 
 export default App;
